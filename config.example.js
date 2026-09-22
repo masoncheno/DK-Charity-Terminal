83bebf4a3bd8ac7a9f5abf41537af063de525366
masoncheno <mascheno@iu.edu>
2026-09-22 17:27:32 -0400

    Delete config.example.js

--- config.example.js
+++
@@ -1,7 +0,0 @@
-// DO NOT replace your working config.js with this unless you need a template.
-window.BT_CONFIG = {
-  SUPABASE_URL: "https://YOUR-PROJECT.supabase.co/rest/v1/",
-  SUPABASE_ANON_KEY: "YOUR_PUBLIC_ANON_KEY",
-  ROOM_CODE: "FRIENDS-1",
-  ESPN_REFRESH_MS: 60000
-};
